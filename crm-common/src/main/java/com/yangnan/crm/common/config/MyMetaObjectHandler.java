6779760b87b9f12b1d80e39573877ac299197966
yangcunnixige <1721380260@qq.com>
2023-06-09 09:57:39 +0800

    分页配置

---
+++ crm-common/src/main/java/com/yangnan/crm/common/config/MyMetaObjectHandler.java
@@ -0,0 +1,22 @@
+package com.yangnan.crm.common.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        // 插入时，创建时间和修改时间
+        this.setFieldValByName("gmtCreate", new Date(), metaObject);
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
+    }
+}
