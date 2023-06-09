b009eb864f6cfd1870d5c12b6bb32ad31a2a0454
yangcunnixige <1721380260@qq.com>
2023-06-09 14:33:21 +0800

    学生管理

--- crm-rbac/src/main/java/com/yangnan/crm/rbac/MPGenerator.java
+++ crm-rbac/src/main/java/com/yangnan/crm/rbac/MPGenerator.java
@@ -16,7 +16,7 @@ import com.baomidou.mybatisplus.generator.fill.Property;
 
 public class MPGenerator {
     //需要配置
-    private static final String dbTables = "user";             //需要生成的表名
+    private static final String dbTables = "students";             //需要生成的表名
     private static final Boolean enableSwagger = false;                 //是否开启Swagger
 
     public static void main(String[] args) {
