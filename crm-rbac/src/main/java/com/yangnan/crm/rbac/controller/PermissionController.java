4b1bb03f0ce3363f43a88b0db635a17bc98cdb13
yangcunnixige <1721380260@qq.com>
2023-06-14 08:27:34 +0800

    权限

--- crm-rbac/src/main/java/com/yangnan/crm/rbac/controller/PermissionController.java
+++ crm-rbac/src/main/java/com/yangnan/crm/rbac/controller/PermissionController.java
@@ -1,16 +1,16 @@
 package com.yangnan.crm.rbac.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.yangnan.crm.common.util.JSONResult;
+import com.yangnan.crm.rbac.pojo.Permission;
 import com.yangnan.crm.rbac.pojo.vo.PermissionVO;
 import com.yangnan.crm.rbac.service.IPermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * <p>
@@ -31,4 +31,33 @@ public class PermissionController {
         List<PermissionVO> list = permissionService.selectAll();
         return JSONResult.ok(list);
     }
+
+    @DeleteMapping("/deleteById/{id}")
+    public JSONResult deleteById(@PathVariable("id") Long id) {
+        boolean isSuccess = permissionService.removeById(id);
+        return isSuccess == true ? JSONResult.ok("删除成功") : JSONResult.error("删除失败");
+    }
+
+    @PostMapping("/add")
+    public JSONResult add(@RequestBody Permission permission) {
+        System.out.println("permission: " + permission);
+        boolean isSuccess = permissionService.save(permission);
+        //boolean isSuccess = userService.saveOrUpdate(user);
+        return isSuccess == true ? JSONResult.ok("添加成功") : JSONResult.error("添加失败");
+    }
+
+    @GetMapping("/selectById/{id}")
+    public JSONResult selectById(@PathVariable("id") Long id) {
+        QueryWrapper wrapper = new QueryWrapper();
+        wrapper.eq("id", id);
+        Permission permission = permissionService.getOne(wrapper);
+        return JSONResult.ok(permission);
+    }
+
+    @PutMapping("/update")
+    public JSONResult update(@RequestBody Permission permission) {
+        boolean isSuccess = permissionService.updateById(permission);
+        return isSuccess == true ? JSONResult.ok("更新成功") : JSONResult.error("更新失败");
+    }
+
 }
